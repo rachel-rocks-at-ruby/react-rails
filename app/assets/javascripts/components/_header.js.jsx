4fcee190699de6c54f832df66e09f916e1ad4047
rachel-rocks-at-ruby <rachelb@gmail.com>
2016-08-16 18:40:40 -0700

    styling changes

--- app/assets/javascripts/components/_header.js.jsx
+++ app/assets/javascripts/components/_header.js.jsx
@@ -3,6 +3,7 @@ var Header = React.createClass({
 		return (
 			<div>
 				<h1>Rachel's React & Rails Project</h1>
+				<h6>List your favorite TV show and your favorite thing about it</h6>
 			</div>
 		)
 	}
